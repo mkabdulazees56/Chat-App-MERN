3b78402bf7d531298f004ded33685e44e01d5b51
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-10-11 08:48:56 +0530

    JWT token created

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,3 +1,4 @@
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
@@ -14,11 +15,61 @@ export const signup = async (req, res) => {
 
     // hash password
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ fullname, email, password:hashPassword });
+    const newUser = new User({ fullname, email, password: hashPassword });
     await newUser.save();
-    res.status(201).json({ messge: "User created successfully" });
+    if (newUser){
+      createTokenAndSaveCookie(newUser._id,res);
+      res.status(201).json({ message: "User created successfully",newUser });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(400).json({ error: "Invalid user credential" });
+    }
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json({
+      message: "User logged in successfully",
+      user: {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(201).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// export const allUsers = async (req, res) => {
+//   try {
+//     const loggedInUser = req.user._id;
+//     const filteredUsers = await User.find({
+//       _id: { $ne: loggedInUser },
+//     }).select("-password");
+//     res.status(201).json(filteredUsers);
+//   } catch (error) {
+//     console.log("Error in allUsers Controller: " + error);
+//   }
+// };
+
+
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {
   //allUsers,
-  //login,
-  //logout,
+  login,
+  logout,
   signup,
 } from "../controller/user.controller.js";
 //import secureRoute from "../middleware/secureRoute.js";
 const router = express.Router();
 
- router.post("/signup", signup);
-//router.post("/login", login);
-//router.post("/logout", logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 //router.get("/allusers", secureRoute, allUsers);
 
 export default router;
